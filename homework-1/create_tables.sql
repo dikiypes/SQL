2b60950cc3944d6e59f6ee9f3ee7c6ab45436b75
Evgeniy <Darkorbitint1@yandex.ru>
2023-10-03 19:59:02 +0300

    121212

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,22 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(employee_id INT PRIMARY KEY,
+first_name TEXT,
+last_name TEXT,
+title TEXT,
+birth_date DATE,
+notes TEXT);
+
+CREATE TABLE customers(
+    customer_id TEXT PRIMARY KEY,
+    company_name TEXT,
+    contact_name TEXT,
+   );
+
+CREATE TABLE orders(
+    order_id INT PRIMARY KEY,
+    customer_id TEXT,
+    employee_id	 INT,
+    order_date INT,
+    ship_city TEXT,);
+
+
